8520f1a02de4f78a89b2a107718f03c950294f1d
kawaMK2 <shintarooo0079@gmail.com>
2017-05-11 16:10:27 +0900

    Improved Top page

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # NAL研 卒研ノート
 
-Python 3.6.1
+Python 3.6.1  
+Django 1.11.1
   
 
 管理者アカウント  
--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Tag, Note
-from accounts.models import User
+from accounts.models import *
 
 
 # Create your views here.
+class GradeTable(object):
+    def __init__(self, year, user_table=[]):
+        self.year = year
+        self.user_table = user_table
+
+
+class UserTable(object):
+    def __init__(self, grade, users=[]):
+        self.grade = grade
+        self.users = users
+
+
 def top(request):
-    return render(request, "notes/top.html", {"users": User.objects.all(), "tags": Tag.objects.all()})
+    # 年度の一覧表を作る
+    years = dict([(b.start_time.year, 0) for b in Belong.objects.all()])
+    year_list = years.keys()
+    year_list = sorted(year_list, reverse=True)
+
+    # 年度毎のテーブルを作る
+    grade_tables = []
+    for year in year_list:
+        user_tables = []
+        for grade in Grade.objects.all():
+            grade_name = grade.formal_name
+            user_belongs = Belong.objects.filter(start_time__year=year, grade__formal_name=grade_name)
+            users = []
+            for belong in user_belongs:
+                users.append(belong.user)
+            user_tables.append(UserTable(grade, users))
+        grade_tables.append(GradeTable(year, user_tables))
+
+    dictionary = {'grade_tables': grade_tables, 'tags': Tag.objects.all()}
+    return render(request, "notes/top.html", dictionary)
 
 
 def note(request, note_title):
