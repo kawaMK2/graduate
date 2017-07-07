62073af75e2c9dcca2f68f5ea7ab17c6109fb8a8
kawaMK2 <shintarooo0079@gmail.com>
2017-07-07 14:56:54 +0900

    Remove login.html comment

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,9 @@ from imagekit.processors import ResizeToFill
 
 # Create your models here.
 class Grade(models.Model):
+    def __init__(self):
+        pass
+
     name = models.CharField(max_length=100)
     formal_name = models.CharField(max_length=100)
     priority = models.IntegerField()
